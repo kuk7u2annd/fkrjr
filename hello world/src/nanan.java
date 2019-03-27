b5bba9474c053ba67f36c0b2525907dafd18bbc3
mean <kuk7775@daum.net>
2019-03-27 11:39:27 +0900

    dfa

--- hello world/src/nanan.java
+++ hello world/src/nanan.java
@@ -3,5 +3,6 @@ public class nanan {
 	public static void main(String[] args) {
 	      System.out.println("Hello World");
 	      System.out.println("apple");
+	      System.out.println("apples");
 	   }
 }
