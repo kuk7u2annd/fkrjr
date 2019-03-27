daf108f5d1db5643daaa09d447ffe56c346de6ca
mean <kuk7775@daum.net>
2019-03-27 12:05:57 +0900

    saf

--- hello world/src/nanan.java
+++ hello world/src/nanan.java
@@ -3,6 +3,7 @@ public class nanan {
 	public static void main(String[] args) {
 	      System.out.println("Hello World");
 	      System.out.println("apple");
+	      System.out.println("apples");
 	      System.out.println("applesdfad");
 	   }
 }
