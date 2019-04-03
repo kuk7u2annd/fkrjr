7295cd5e2fff2e83dfcc343553a01e8c4482f4f0
kuk7u2annd <48500698+kuk7u2annd@users.noreply.github.com>
2019-04-03 10:33:51 +0900

    Update fuckyou.java

--- hello world/src/fuckyou.java
+++ hello world/src/fuckyou.java
@@ -1,4 +1,5 @@
 
 public class fuckyou {
 	public static void main(String[] args) {
-	      System.out.println("youyou");}}
+	      System.out.println("youyou");
+		System.out.println("wha!?!?!?");}}
